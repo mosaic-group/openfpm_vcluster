7445c29c78b727569f2f5c9eb90dcc42d55fdbad
Pietro Incardona <incardon@mpi-cbg.de>
2015-05-12 12:37:55 +0200

    Added jenkins build script

---
+++ build_vcluster.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Make a directory in /tmp/OpenFPM_data
+
+mkdir /tmp/openfpm_vcluster
+mv * .[^.]* /tmp/openfpm_vcluster
+mv /tmp/openfpm_vcluster OpenFPM_vcluster
+
+mkdir OpenFPM_vcluster/src/config
+
+git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_devices
+git clone git@ppmcore.mpi-cbg.de:incardon/openfpm_devices.git OpenFPM_data
+
+cd "$1/OpenFPM_vcluster"
+
+sh ./autogen.sh
+sh ./configure CXX=mpic++
+make
+
+mpirun -np 2 ./src/vcluster
+mpirun -np 4 ./src/vcluster
+mpirun -np 16 ./src/vcluster
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream.h>
+#include "config.h"
+#define BOOST_TEST_MODULE "C++ test module for OpenFPM_vcluster project"
+#include <boost/test/included/unit_test.hpp>
 
-int main()
-{
 
-}
+#include "VCluster_unit_tests.hpp"
