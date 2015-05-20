31d3dfbaec63bef69038f46713a2cc2774e2a7d2
Pietro Incardona <incardon@mpi-cbg.de>
2015-05-20 17:27:24 +0200

    Fixing jenkins script

--- build_vcluster.sh
+++ build_vcluster.sh
@@ -25,19 +25,6 @@ then
  module load openmpi/1.8.1
 fi
 
-if [ "$2" == "wetcluster" ]
-then
- echo "Compiling on wetcluster\n"
- module load gcc/4.9.2
- module load openmpi/1.8.1
- module load boost/1.54.0
- COMPILE_OPTIONS = '--with-boost="/sw/apps/boost/1.54.0/"'
-fi
-
-sh ./autogen.sh
-sh ./configure "$(COMPILE_OPTIONS)"  CXX=mpic++
-make
-
 if [ $2 == "wetcluster" ]
 then
  bsub -K -n 2 mpirun -np 2 ./src/vcluster
@@ -47,4 +34,33 @@ else
 # mpirun -np 2 ./src/vcluster
 # mpirun -np 4 ./src/vcluster
 fi
+
+if [ "$2" == "wetcluster" ]
+then
+ echo "Compiling on wetcluster"
+
+## produce the module path
+
+export MODULEPATH="/sw/apps/modules/modulefiles:$MODULEPATH"
+
+ script="module load gcc/4.9.2\n 
+module load openmpi/1.8.1\n
+module load boost/1.54.0\n
+compile_options='--with-boost=/sw/apps/boost/1.54.0/'\n
+\n
+sh ./autogen.sh\n
+sh ./configure \"\$compile_options\"  CXX=mpic++\n
+make\n
+if [ \"\$?\" = "0" ]; then exit 1 ; fi\n
+exit(0)\n"
+
+ echo $script | sed -r 's/\\n/\n/g' > compile_script
+
+# bsub  "cd \"$1/OpenFPM_vcluster\"\n
+#                          sh ./compile_script"
+
+# bsub -K -n 2 mpirun -np 2 ./src/vcluster
+# if [ "$?" = "0" ]; then exit 1 ; fi
+fi
+
 
