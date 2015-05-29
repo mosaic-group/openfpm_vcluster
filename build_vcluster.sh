028ce9f47614bf0d250a64886c8a4612792f05ba
Pietro Incardona <incardon@mpi-cbg.de>
2015-05-29 19:49:46 +0200

    Changed the position of MPI_Init

--- build_vcluster.sh
+++ build_vcluster.sh
@@ -74,15 +74,15 @@ then
  make
  if [ $? -ne 0 ]; then exit 1 ; fi
 
- salloc --nodes=1 --ntasks-per-node=16 --time=04:00:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 16 src/vcluster
+ salloc --nodes=1 --ntasks-per-node=16 --time=00:05:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 16 src/vcluster
  if [ $? -ne 0 ]; then exit 1 ; fi
- salloc --nodes=2 --ntasks-per-node=16 --time=04:00:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 32 src/vcluster
+ salloc --nodes=2 --ntasks-per-node=16 --time=00:05:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 32 src/vcluster
  if [ $? -ne 0 ]; then exit 1 ; fi
- salloc --nodes=4 --ntasks-per-node=16 --time=04:00:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 64 src/vcluster
+ salloc --nodes=4 --ntasks-per-node=16 --time=00:05:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 64 src/vcluster
  if [ $? -ne 0 ]; then exit 1 ; fi
- salloc --nodes=8 --ntasks-per-node=16 --time=04:00:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 128 src/vcluster
+ salloc --nodes=8 --ntasks-per-node=16 --time=00:05:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 128 src/vcluster
  if [ $? -ne 0 ]; then exit 1 ; fi
- salloc --nodes=16 --ntasks-per-node=16 --time=04:00:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 256 src/vcluster
+ salloc --nodes=16 --ntasks-per-node=16 --time=00:05:00 --mem-per-cpu=1900 --partition=sandy mpirun -np 256 src/vcluster
  if [ $? -ne 0 ]; then exit 1 ; fi
 
 else
