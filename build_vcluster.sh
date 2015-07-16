81d5b56f6c901c73b03a41901151097300aa73c0
Pietro Incardona <incardon@mpi-cbg.de>
2015-07-16 17:17:11 +0200

    Script fixing for taurus2

--- build_vcluster.sh
+++ build_vcluster.sh
@@ -69,8 +69,8 @@ then
  echo "$PATH"
  module load gcc/4.8.2
  module load boost/1.55.0-gnu4.8
+ module load openmpi/1.8.7
  module unload bullxmpi
- source ~/.bashrc
 
  sh ./autogen.sh
  sh ./configure --enable-verbose CXX=mpic++
