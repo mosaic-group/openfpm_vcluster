730d7f34d280e69c68f6ee1751b1e1ae118b2e17
Pietro Incardona <i-bird@localhost.localdomain>
2016-03-18 02:30:11 +0100

    Fixing taurus request

--- build_vcluster.sh
+++ build_vcluster.sh
@@ -76,19 +76,19 @@ then
 
 ### to exclude --exclude=taurusi[6300-6400],taurusi[5400-5500]
 
- salloc --nodes=1 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1900 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 24 src/vcluster --report_level=no"
+ salloc --nodes=1 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1800 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 24 src/vcluster --report_level=no"
  if [ $? -ne 0 ]; then exit 1 ; fi
  sleep 5
- salloc --nodes=2 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1900 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 48 src/vcluster --report_level=no"
+ salloc --nodes=2 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1800 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 48 src/vcluster --report_level=no"
  if [ $? -ne 0 ]; then exit 1 ; fi
  sleep 5
- salloc --nodes=4 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1900 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 96 src/vcluster --report_level=no"
+ salloc --nodes=4 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1800 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 96 src/vcluster --report_level=no"
  if [ $? -ne 0 ]; then exit 1 ; fi
  sleep 5
- salloc --nodes=8 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1900 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 192 src/vcluster --report_level=no"
+ salloc --nodes=8 --ntasks-per-node=24 --time=00:05:00 --mem-per-cpu=1800 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 192 src/vcluster --report_level=no"
  if [ $? -ne 0 ]; then exit 1 ; fi
  sleep 5
- salloc --nodes=10 --ntasks-per-node=24 --time=00:5:00 --mem-per-cpu=1900 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 240 src/vcluster --report_level=no"
+ salloc --nodes=10 --ntasks-per-node=24 --time=00:5:00 --mem-per-cpu=1800 --partition=haswell bash -c "ulimit -s unlimited && mpirun -np 240 src/vcluster --report_level=no"
  if [ $? -ne 0 ]; then exit 1 ; fi
 
 else
