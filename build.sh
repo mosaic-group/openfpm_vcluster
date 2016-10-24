bad924ac777805de0414310c62d5e9c90d6efde6
Pietro Incardona <incardon@mpi-cbg.de>
2016-10-24 02:12:38 +0200

    Fixing address

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ mv /tmp/openfpm_vcluster openfpm_vcluster
 
 mkdir openfpm_vcluster/src/config
 
-git clone git@git.mpi-cbg.de:/incardon/openfpm_devices.git openfpm_devices
-git clone git@git.mpi-cbg.de:/incardon/openfpm_data.git openfpm_data
+git clone git@git.mpi-cbg.de:/openfpm/openfpm_devices.git openfpm_devices
+git clone git@git.mpi-cbg.de:/openfpm/openfpm_data.git openfpm_data
 
 cd "$1/openfpm_vcluster"
 
