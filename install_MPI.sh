1fdaace82b6b3a96fd33c5f710f40562be71f7c7
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-18 16:22:57 +0100

    Adding configure and install MPI script

--- install_MPI.sh
+++ install_MPI.sh
@@ -1,9 +1,41 @@
-#!/bin/bash 
+#!/bin/bash
 
-mkdir ${HOME}/MPI
-wget http://www.open-mpi.de/software/ompi/v1.8/downloads/openmpi-1.8.7.tar.bz2
-tar -xvf openmpi-1.8.7.tar.bz2
-cd openmpi-1.8.7
-sh ./configure --prefix=${HOME}/MPI --enable-opal-multi-threads --enable-mpi-f90
-make
+# check if the directory $1/MPI exist
+
+if [ -d "$1/MPI" ]; then
+  echo "MPI already installed"
+  exit 0
+fi
+rm -rf openmpi-3.1.1
+rm openmpi-3.1.1.tar.gz
+wget http://ppmcore.mpi-cbg.de/upload/openmpi-3.1.1.tar.gz
+tar -xvf openmpi-3.1.1.tar.gz
+cd openmpi-3.1.1
+
+#
+#                  --disable-mca-dso \
+#                 --disable-sysv-shmem \
+#                 --enable-cxx-exceptions \
+#                 --with-threads=posix \
+#                 --without-cs-fs \
+#                 --with-mpi-param_check=always \
+#                 --enable-contrib-no-build=vt,libompitrace \
+#
+#--enable-mca-no-build=paffinity,installdirs-windows,timer-windows,shmem-sysv
+#
+#
+
+
+if [ x"$6" == x"1" ]; then
+   echo "Installing MPI with GPU support"
+  ./configure --with-cuda --prefix=$1/MPI --enable-mpi-fortran=yes CC=$3 CXX=$4 F77=$5 FC=$5
+else
+  echo "Installing MPI without GPU support"
+  ./configure --prefix=$1/MPI --enable-mpi-fortran=yes CC=$3 CXX=$4 F77=$5 FC=$5
+fi
+make -j $2
 make install
+
+# Mark the installation
+echo 3 > $1/MPI/version
+
