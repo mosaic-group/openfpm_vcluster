63f417783616e320fff765a8970992825175af59
Pietro Incardona <incardon@mpi-cbg.de>
2018-11-25 00:23:20 +0100

    Fixing boost test full dynamic

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE "C++ test module for OpenFPM_vcluster project"
 #define BOOST_TEST_DYN_LINK
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "config.h"
 #include "VCluster/VCluster.hpp"
