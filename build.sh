9ee96b99f4b18890172da4d4823b6f6ec991894d
Pietro Incardona <incardon@mpi-cbg.de>
2019-06-09 19:05:31 +0200

    Eliminating ghost process again

--- build.sh
+++ build.sh
@@ -38,7 +38,7 @@ if [ ! -d $HOME/openfpm_dependencies/openfpm_vcluster/LIBHILBERT ]; then
         ./install_LIBHILBERT.sh $HOME/openfpm_dependencies/openfpm_vcluster/ 4
 fi
 
-if [ x"$hostname" == x"cifarm-mac-node.mpi-cbg.de" ]; then
+if [ x"$hostname" == x"cifarm-mac-node" ]; then
 	echo "Killing ghost"
 	kill 73440 87662 87661 73439 51687 51686
 fi
