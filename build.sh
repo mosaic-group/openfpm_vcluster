d135df3dbbc23a905d470e660c75282573b88c23
Incardona Pietro <incardon@mpi-cbg.de>
2021-03-17 22:22:53 +0100

    changinng to develop for testing

--- build.sh
+++ build.sh
@@ -24,10 +24,10 @@ mkdir openfpm_vcluster/src/config
 git clone git@git.mpi-cbg.de:/openfpm/openfpm_devices.git openfpm_devices
 git clone git@git.mpi-cbg.de:/openfpm/openfpm_data.git openfpm_data
 cd openfpm_data
-git checkout master
+git checkout develop
 cd ..
 cd openfpm_devices
-git checkout master
+git checkout develop
 cd ..
 
 cd "$workspace/openfpm_vcluster"
